a040a869ff1850f97537874de1932a876d9e6ee7
sarori <salo3678@gmail.com>
2021-01-05 08:26:56 +0100

    day04

---
+++ JANUARY/04/mergeTwoLists.c
@@ -0,0 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function (l1, l2) {
+	if (!l1) return l2
+	if (!l2) return l1
+
+	let dummyNode = new ListNode(-1)
+	let res = dummyNode
+
+	while (l1 && l2) {
+		if (l1.val > l2.val) {
+			dummyNode.next = new ListNode(l2.val)
+			dummyNode = dummyNode.next
+			l2 = l2.next
+		} else if (l1.val < l2.val) {
+			dummyNode.next = new ListNode(l1.val)
+			dummyNode = dummyNode.next
+			l1 = l1.next
+		} else {
+			dummyNode.next = new ListNode(l2.val)
+			dummyNode = dummyNode.next
+			l2 = l2.next
+
+			dummyNode.next = new ListNode(l1.val)
+			dummyNode = dummyNode.next
+			l1 = l1.next
+		}
+	}
+	if (l1) {
+		dummyNode.next = l1
+	}
+	if (l2) {
+		dummyNode.next = l2
+	}
+	return res.next
+}
